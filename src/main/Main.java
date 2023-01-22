726c2c9348e81e52c4d61b15c8c334b9a8038e07
Gorkem Muslu <muslu.gorkem@gmail.com>
2023-01-22 23:50:30 +0300

    updated command design pattern

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+import command.CancelOrderCommand;
+import command.Command;
+import command.Invoker;
+import command.Order;
+import command.ProcessOrderCommand;
 import observer.Company;
 import observer.GoldUser;
 import observer.RegularUser;
@@ -9,7 +14,7 @@ import observer.User;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Case 1: ");
+        System.out.println("Case 1 (Observer): ");
         System.out.println("-------------------------------");
         Company thy = new THY();
         User regularUser = new RegularUser("John Doe");
@@ -19,6 +24,22 @@ public class Main {
         thy.addObserver(goldUser);
         thy.notifyObservers();
         System.out.println("-------------------------------");
-        System.out.println("Case 2: ");
+        System.out.println("Case 2 (Command): ");
+
+        Order order = new Order("T-shirt");
+
+        Command processCommand = new ProcessOrderCommand(order);
+        Command cancelCommand = new CancelOrderCommand(order);
+
+        Invoker invoker = new Invoker();
+        invoker.setCommand(processCommand);
+        invoker.executeCommand();
+        System.out.println("Client Message: Order processed.");
+
+        invoker.setCommand(cancelCommand);
+        invoker.executeCommand();
+        System.out.println("Client Message: Order cancelled.");
+        System.out.println("-------------------------------");
+
     }
 }
